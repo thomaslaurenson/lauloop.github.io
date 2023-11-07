2ea96d42b977460018afc550e1323b7509e30bd7
Thomas Laurenson <thomas@thomaslaurenson.com>
2023-11-08 08:32:27 +1300

    Updated packages

--- src/Home.tsx
+++ src/Home.tsx
@@ -8,13 +8,9 @@ function Home() {
       direction="column"
       justifyContent="center"
       alignItems="center"
-    //   sx={{ minHeight: "100%" }}
       >
       <Grid item>
         <Typography variant="h2">lauloop</Typography>
-      </Grid>
-      <Grid item>
-      <Typography variant="h5">coming soon</Typography>
       </Grid>
     </Grid>
   )
